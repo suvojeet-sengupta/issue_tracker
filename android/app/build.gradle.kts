f488208a1ae3301d51abf57febe56a00fbe8ae31
suvojit213 <suvojitsengupta21@gmail.com>
2025-07-03 21:55:04 +0530

    fix: Update compileSdk to 35 for plugin compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,8 +7,8 @@ plugins {
 
 android {
     namespace = "com.suvojeet.issue_tracker_app"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 35
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
