5e6f84a9671873de36f8ba6374cae16ef34068f6
suvojit213 <suvojitsengupta21@gmail.com>
2025-07-19 12:46:44 +0530

    chore: Update app version to 1.0.4
    Updated the app version to 1.0.4 across , , and .

--- lib/about_app_screen.dart
+++ lib/about_app_screen.dart
@@ -40,7 +40,7 @@ class AboutAppScreen extends StatelessWidget {
                     ),
                   ),
                   const Text(
-                    'Version 1.0.3',
+                    'Version 1.0.4',
                     style: TextStyle(
                       fontSize: 16,
                       color: Colors.grey,
