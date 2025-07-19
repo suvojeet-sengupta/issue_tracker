8b4428d886c87cbdbc0e9e80c844482e0f23e6f5
suvojit213 <suvojitsengupta21@gmail.com>
2025-07-19 09:43:47 +0530

    fix: Remove duplicated code in notification history screen
    Removed a duplicated code block within  that was causing multiple syntax errors and build failures. This rectifies the previous attempt to fix the  widget implementation.

--- lib/notification_history_screen.dart
+++ lib/notification_history_screen.dart
@@ -198,7 +198,7 @@ class _NotificationHistoryScreenState extends State<NotificationHistoryScreen> {
                                   ),
                                 ),
                               );
-                                  
+
                             },
                           ),
               ),
