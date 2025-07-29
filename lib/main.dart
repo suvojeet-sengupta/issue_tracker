73727599e59b5e99c326a8efc77b5ce385875c47
suvojit213 <suvojitsengupta21@gmail.com>
2025-07-29 13:43:22 +0530

    fix: Upgrade firebase_messaging to resolve dependency conflict

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-'''import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
@@ -100,7 +100,7 @@ class _MyAppState extends State<MyApp> {
     } on PlatformException catch (e) {
       print("Failed to schedule notifications: '${e.message}'.");
     }
-  }''
+  }
 
   @override
   Widget build(BuildContext context) {
