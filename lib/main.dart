dd9c232738da5e9104aa78e62dad4c0ecb0b2ab5
suvojit213 <suvojitsengupta21@gmail.com>
2025-07-29 16:07:11 +0530

    fix: Resolve MethodChannel and PlatformException errors

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
